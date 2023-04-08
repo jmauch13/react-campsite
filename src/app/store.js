2f4063ba40fcf92bd3cda1eb03e3407ec1205802
Jennie Mauch <jenniemauch@me.com>
2023-04-08 16:21:59 -0400

    login

--- src/app/store.js
+++ src/app/store.js
@@ -3,13 +3,15 @@ import { campsitesReducer } from '../features/campsitesSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { promotionsReducer} from '../features/promotions/promotionsSlice';
+import { userReducer } from '../features/user/userSlice';
 
 export const store = configureStore({
     reducer: {
         campsites: campsitesReducer,
         comments: commentsReducer,
         partners: partnersReducer,
-        promotions: promotionsReducer
+        promotions: promotionsReducer,
+        user: userReducer
     }
 });
 
